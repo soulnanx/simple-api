9c30c9f726496afb12d236685bea78bf2c2906ce
Renan dos Santos <renan.esantos@mercadolivre.com>
2025-04-13 17:57:06 -0300

    Rotas basicas do Tasks

---
+++ main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	IsCompleted bool   `json:"is_completed"`
+}
+
+var (
+	tasks  = []Task{}
+	nextID = 1
+	mu     sync.Mutex
+)
+
+func main() {
+
+	http.HandleFunc("/tasks", tasksHandler)
+
+	http.HandleFunc("/ping", pingHandler)
+
+	port := "3000"
+	fmt.Printf("Servidor rodando na porta %s...\n", port)
+	http.ListenAndServe(":"+port, nil)
+}
+
+func tasksHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getTasks(w, r)
+	case "POST":
+		addTask(w, r)
+	case "DELETE":
+		deleteTask(w, r)
+	default:
+		http.Error(w, "Método não suportado", http.StatusMethodNotAllowed)
+	}
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	ping(w, r)
+}
+
+func ping(w http.ResponseWriter, _ *http.Request) {
+	fmt.Fprintf(w, "pong")
+}
+
+func getTasks(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func addTask(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	var newTask Task
+	if err := json.NewDecoder(r.Body).Decode(&newTask); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	newTask.ID = nextID
+	nextID++
+	newTask.IsCompleted = false
+	tasks = append(tasks, newTask)
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	// Para simplicidade, vamos deletar a tarefa pelo ID presente no caminho
+	taskID := r.URL.Query().Get("id")
+	for i, task := range tasks {
+		if task.Title == taskID {
+			tasks = append(tasks[:i], tasks[i+1:]...) // Remove a tarefa
+			w.WriteHeader(http.StatusNoContent)       // Status 204 No Content
+			return
+		}
+	}
+
+	http.NotFound(w, r) // Retorna 404 se a tarefa não for encontrada
+}
